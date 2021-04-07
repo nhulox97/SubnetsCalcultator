85132baf431acd2012cd76f70ede105b8f2fccc5
Sergio Bernal <sergiobernal909@gmail.com>
2021-04-07 17:44:42 -0600

    change typo language from es to en

--- main.py
+++ main.py
@@ -3,21 +3,26 @@
 # el split.
 
 
-# logica para seprar octetos de la ip
+# logica para seprar octets de la ip
 # input ip inicial
 ip_inicial = '174.40.6.0'
-host = 3
-octetos = ip_inicial.split('.')
-octetos = [int(octeto) for octeto in octetos]
+last_octet_position = 3
+octets = ip_inicial.split('.')
+octets = [int(octet) for octet in octets]
 host_solicitados = [300, 200, 110, 60, 10, 2, 2, 2]
 host_len = len(host_solicitados)
 pesos = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
 host_encontrados = 0
 
 
-def get_mascara_de_red(peso):
+def get_network_mask(bit_weight: int = 1):
+    """
+    Get the network mask based on the bits weight
+    :position bit_weight: int
+    :return network_mask: list
+    """
     mask_string = ''
-    mask_bits = 32 - peso
+    mask_bits = 32 - bit_weight
     for bit in range(0, 32, 1):
         if bit >= mask_bits:
             mask_string += '0'
@@ -26,107 +31,107 @@ def get_mascara_de_red(peso):
         if (bit + 1) % 8 == 0 and bit < 31:
             mask_string += '.'
 
-    # convertir octetos a decimal
+    # convert binary mask octets to decimal
     mask_octetos = mask_string.split('.')
     mask_decimal = ''
     position_mask = 0
     for mask_octeto in mask_octetos:
-        digito_d = 0
-        for position, digito_s in enumerate(mask_octeto[::-1]):
-            digito_d += int(digito_s) * 2 ** position
-        # concatenar el valor decimal del octeto
-        mask_decimal += f'{digito_d}'
+        digit_decimal = 0
+        for position, digit_string in enumerate(mask_octeto[::-1]):
+            digit_decimal += int(digit_string) * 2 ** position
+        # concat decimal value to decimal mask
+        mask_decimal += f'{digit_decimal}'
         if position_mask < 3:
             mask_decimal += '.'
         position_mask += 1
 
-    #concatenar bits de red
+    # concat network bits
     mask_decimal += f'/{mask_bits}'
 
     return [mask_string, mask_decimal]
 
 
-def concat_octetos(c_octetos: list, type: str = ''):
-    octeto_ultimo = c_octetos[3]
-    if (type == 'p'):
-        octeto_ultimo += 1
-    if (type == 'u'):
-        octeto_ultimo -= 1
+def concat_octets(c_octet: list, position: str = ''):
+    last_octet = c_octet[3]
+    if position == 'p':
+        last_octet += 1
+    if position == 'u':
+        last_octet -= 1
 
-    return f'{c_octetos[0]}.{c_octetos[1]}.{c_octetos[2]}.{octeto_ultimo}'
+    return f'{c_octet[0]}.{c_octet[1]}.{c_octet[2]}.{last_octet}'
 
 
 def print_subnets(lsubnet):
     print('\hline %18s & %15s & %15s & %15s & %15s \\\\' %
-          ('Mascar de red','Ip de red', 'Primera IP', 'Ultima IP', 'Ip Broadcast'))
+          ('Mascara de red', 'Ip de red', 'Primera IP', 'Ultima IP', 'Ip Broadcast'))
     for subnet in subnets:
-        mask, red, primera, ultima, broadcast = subnet
-        print('\hline %18s & %15s & %15s & %15s & %15s \\\\' % (mask, red, primera, ultima, broadcast))
+        mask, red, primera, ultima, broadcast_ip = subnet
+        print('\hline %18s & %15s & %15s & %15s & %15s \\\\' % (mask, red, primera, ultima, broadcast_ip))
 
 
-counter = 0
 subnets = [[]]
 
-for host_solicitado in host_solicitados:
-    # print('=' * 80)
-    # encontrar los hosts solicitados
-    peso_index = 0
-    for peso in pesos:
+for requested_host_index, host_solicitado in enumerate(host_solicitados):
+    # declare binary and decimal mask
+    mask_b, mask_d = '', ''
+    # find requested hosts
+    for peso_i, peso in enumerate(pesos):
         peso_encontrado = peso - 2
         if peso_encontrado >= host_solicitado:
             host_encontrados = peso_encontrado
+            # get network mask
+            mask_b, mask_d = get_network_mask(peso_i)
             break
-        peso_index += 1
 
-    # obetener mascara de red
-    mask_b, mask_d = get_mascara_de_red(peso_index)
-    # agregar mascara de red
-    subnets[counter].append(mask_d)
-    # agregar ip de direccion red
-    subnets[counter].append(concat_octetos(octetos))
-    # aregar primera ip de
-    subnets[counter].append(concat_octetos(octetos, 'p'))
-    # sumar los bits de hosts a la ip
+    # append the mask ip
+    subnets[requested_host_index].append(mask_d)
+    # append the network ip
+    subnets[requested_host_index].append(concat_octets(octets))
+    # append first available ip
+    subnets[requested_host_index].append(concat_octets(octets, 'p'))
+
+    # add last_octet_position bits to ip
     was_added = False
-    counter_interno = 0
+    host_added_counter = 0
     while not was_added:
-        octeto = octetos[host]
-        # saber si la suma del octeto y los hosts es mayor que 255
-        octeto_host = octeto + host_encontrados
+        octet = octets[last_octet_position]
+        octeto_host = octet + host_encontrados
         host_aux = True
         is_added = False
-        # verificar si los hosts encontrados son mayor que 255
+        # check if octet is greater than 255
         if octeto_host > 255:
-            octetos[host - 1] = octetos[host - 1] + 1
-            octetos[host] = 0
-            host_encontrados = 255 - octeto
+            octets[last_octet_position - 1] = octets[last_octet_position - 1] + 1
+            octets[last_octet_position] = 0
+            host_encontrados = 255 - octet
             host_aux = False
             is_added = True
 
         if host_aux:
-            if counter_interno < 1:
-                octetos[host] = octeto_host + 1
+            if host_added_counter < 1:
+                octets[last_octet_position] = octeto_host + 1
             else:
-                octetos[host] = octeto_host
+                octets[last_octet_position] = octeto_host
 
-            # agregar ultima ip disponible
-            subnets[counter].append(concat_octetos(octetos, 'u'))
-            # agregar ip de broadcast
-            subnets[counter].append(concat_octetos(octetos))
-            # print('=' * 80)
+            # append last available ip
+            subnets[requested_host_index].append(concat_octets(octets, 'u'))
+            # append broadcast ip
+            subnets[requested_host_index].append(concat_octets(octets))
             was_added = True
-        counter_interno += 1
+        host_added_counter += 1
 
-    # sumarle + 1 para que el broadcast salga ocupada
-    broadcast = octetos[host] + 1
-    if (broadcast > 255):
-        octetos[host - 1] = octetos[host - 1] + 1
-        octetos[host] = 0
+    # add 1 to last octet to mark previous broadcast ip as unavailable
+    broadcast = octets[last_octet_position] + 1
+
+    # check if the next network ip is greater than 255
+    if broadcast > 255:
+        octets[last_octet_position - 1] = octets[last_octet_position - 1] + 1
+        octets[last_octet_position] = 0
     else:
-        octetos[host] = broadcast
+        octets[last_octet_position] = broadcast
 
-    counter += 1
-    if (counter < host_len):
+    if requested_host_index < (host_len - 1):
         subnets.append([])
 
-print_subnets(subnets)
+
+if __name__ == '__main__':
+    print_subnets(subnets)
